73de6641a706dc627ca2edbb1bbcea3a1820d1db
Dan Lin <daniel.lin@musth.io>
2022-12-14 09:51:09 -0800

    Starter
    Signed-off-by: Dan Lin <daniel.lin@musth.io>

---
+++ frontend/.next/static/webpack/webpack.87e0d777965fb6534a1d.hot-update.js
@@ -0,0 +1,12 @@
+self["webpackHotUpdate_N_E"]("webpack",{},
+/******/ function(__webpack_require__) { // webpackRuntimeModules
+/******/ "use strict";
+/******/ 
+/******/ /* webpack/runtime/getFullHash */
+/******/ !function() {
+/******/ 	__webpack_require__.h = function() { return "eda9ad49d405f4b85f27"; }
+/******/ }();
+/******/ 
+/******/ }
+);
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9fTl9FL3dlYnBhY2svcnVudGltZS9nZXRGdWxsSGFzaCJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiOzs7Ozs7VUFBQSxvQ0FBb0MsK0JBQStCLEUiLCJmaWxlIjoic3RhdGljL3dlYnBhY2svd2VicGFjay44N2UwZDc3Nzk2NWZiNjUzNGExZC5ob3QtdXBkYXRlLmpzIiwic291cmNlc0NvbnRlbnQiOlsiX193ZWJwYWNrX3JlcXVpcmVfXy5oID0gZnVuY3Rpb24oKSB7IHJldHVybiBcImVkYTlhZDQ5ZDQwNWY0Yjg1ZjI3XCI7IH0iXSwic291cmNlUm9vdCI6IiJ9
